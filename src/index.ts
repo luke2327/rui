40b91eb33d152ced3e230d6d773796741d873ce8
luke2327 <qwepoi8526@gmail.com>
2020-03-10 17:25:32 +0900

    init 부분 보완

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,26 @@
 import { Client } from 'discord.js';
+import { commonLogFileLocation } from './config/fileStream';
+import { BOT_NAME, VERSION } from './config/settings';
+
 import receiver from './scripts/receiver';
+import log from './scripts/utils/logUtils';
 
 /** create new Client object */
 const client = new Client();
 
+client.once('ready', () => {
+  log.logger('info', commonLogFileLocation, `BOT_NAME => ${BOT_NAME} | VERSION => ${VERSION} | STATUS => ready`);
+});
+
+client.once('reconnecting', () => {
+  log.logger('warn', commonLogFileLocation, `BOT_NAME => ${BOT_NAME} | VERSION => ${VERSION} | STATUS => reconnecting`);
+});
+
+client.once('disconnect', () => {
+  log.logger('warn', commonLogFileLocation, `BOT_NAME => ${BOT_NAME} | VERSION => ${VERSION} | STATUS => disconnect`);
+});
+
 /** receive */
 receiver.init(client);
 
-client.login('');
+client.login('Njc4MjE4NDQ1MzE3MzQxMTk3.Xmbe2w.eA_I4Qty6-jSll-necB2JxhcHVY');
